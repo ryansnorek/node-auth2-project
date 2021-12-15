583b734b4a7721fd121bf556a38c05a701a91b24
Ryan <snorekr@gmail.com>
2021-12-15 11:01:07 -0500

    done

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -33,8 +33,8 @@ const restricted = (req, res, next) => {
 }
 
 const only = role_name => (req, res, next) => {
-  const { role } = req.decodedToken.role_name
-  if (role_name === role) {
+  req.decodedToken.role_name
+  if (role_name === req.decodedToken.role_name) {
     next()
   } else {
     next({ status: 403, message: "This is not for you" })
